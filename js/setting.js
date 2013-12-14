597c1215d88abfdf63bcd368c703deb9f578d54e
Yutaka Kubota <yutaka.kubota@yui-works.jp>
2013-12-14 16:40:07 +0900

    setting.jsの書式ミスを修正、横浜市の表記が残っていたので逗子市に修正

--- js/setting.js
+++ js/setting.js
@@ -44,7 +44,7 @@ OpenSpending.Styles.Cofog = {
   '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
   '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
   '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
   '11': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
   '11-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
 };
